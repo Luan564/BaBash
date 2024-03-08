e511aafe8a7971375c9cdaa37ec3deeb91a5f63c
Luan564 <1702282A@umich.mx>
2024-03-07 20:12:38 -0600

    first commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+day=`date "+%d"`	#Dia actual del mes
+month=`date "+%B"`	 #Nombre completo del mes
+year=`date "+%y"`	# Año
+time=`date "+%T"`	 #Hora en formato 24hrs
+
+
+if [ "$1" == "hola" ];
+then
+	echo "Hola $2 hoy es `date "+%d"` de `date "+%B"` de `date "+%y"` y son las `date "+%T"` "
+
+elif [ "$1" == "adios" ];
+then
+	echo "Adios $2 hoy es $day de $month de $year y son las $time"
+else
+	echo "Buenas tardes?"
+fi
